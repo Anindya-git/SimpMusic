0685edbd53d7a9073723144d93931a8676fd19aa
Anindya Pandob Rahul <anindya.pandob.rahul@g.bracu.ac.bd>
2026-06-26 13:46:26 +0600

    Add LocalMusicDestination for navigation
    This object represents a type-safe navigation destination for the Local Music screen, intended for use in the navigation graph.

---
+++ composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/navigation/destination/library/LocalMusicDestination.kt
@@ -0,0 +1,12 @@
+package com.maxrave.simpmusic.ui.navigation.destination.library
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Type-safe navigation destination for the Local Music screen.
+ *
+ * Add this to the navigation graph in AppNavigationGraph.kt
+ * alongside the other library destinations (LocalPlaylistDestination, etc.)
+ */
+@Serializable
+object LocalMusicDestination
